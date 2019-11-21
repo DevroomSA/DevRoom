5bb9c12a5a2e6fd6d1ae242dd136f63b224f1218
SM <SM@SIFISO>
2019-11-21 23:14:42 +0200

    latest fixes

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "support@rstheme.com";
+        $recipient = "sifsilva@devroomsa.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
